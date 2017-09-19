3e60e04757793ce1444136c025a3f98a21d5f189
Vladimir Gogunsky <gogunsky.vladimir@gmail.com>
2017-09-20 00:20:05 +0300

    Added test call for competitions

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var axios = require('axios');
 
-app.get('/champs/pl', function (req, res) {
-        fs.readFile( __dirname + "/data/" + "pl_db.json", 'utf8', function (err, data) {
-                    console.log( data );
+axios.defaults.headers.common['Authorization'] = ''
+
+const dataSourceBaseUrl = 'http://api.football-data.org/v1/'
+const competionsUrl = 'competitions?season=2017'
+ 
+
+var cron = require('cron');
+var cronJob = cron.job("0 */10 * * * *", function(){
+                       axios.get(dataSourceBaseUrl + competionsUrl)
+  .then(response => {
+        
+	var obj = JSON.stringify(response.data); 
+        fs.writeFile(__dirname + "/data/" + competionsUrl + ".json", obj);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+                       }); 
+cronJob.start();
+
+
+app.get('/competitions', function (req, res) {
+        fs.readFile( __dirname + "/data/" + competionsUrl + '.json', 'utf8', function (err, data) {
                     res.end( data );
                     });
         })
@@ -17,3 +39,4 @@ var server = app.listen(8080, function () {
                         console.log("Example app listening at http://%s:%s", host, port)
                         
                         })
+
